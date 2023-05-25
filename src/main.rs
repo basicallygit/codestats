2730b52447531f872a9d169a2d8a1a67aebde3a9
basicallygit <91993321+basicallygit@users.noreply.github.com>
2023-05-25 19:50:56 +0100

    Complete re-write

--- src/main.rs
+++ src/main.rs
@@ -1,201 +1,120 @@
-use std::collections::HashMap;
-use std::path::Path;
 use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
+use std::process::exit;
+use std::path::Path;
 use std::fs::File;
 use std::env;
-use std::process::exit;
 
-#[derive(Debug)]
+mod map_extension;
+
+#[derive(Default, Debug)]
 struct Stats {
-    line_count: HashMap<String, usize>, // <lang, count>
-    file_count: HashMap<String, usize>, // <lang, count>
+    linecount: HashMap<String, usize>,
+    filecount: HashMap<String, usize>,
 }
 
 impl Stats {
-    fn new() -> Self {
-        Self {line_count: HashMap::new(), file_count: HashMap::new()}
+    fn new() -> Stats {
+        Default::default()
     }
 
-    fn add_line(&mut self, lang: String) {
-        let count = self.line_count.entry(lang).or_insert(0);
-        *count += 1;
+    #[inline(always)]
+    fn add_lines(&mut self, lang: String, lines: usize) {
+        let count = self.linecount.entry(lang).or_insert(0);
+        *count += lines;
     }
 
+    #[inline(always)]
     fn add_file(&mut self, lang: String) {
-        let count = self.file_count.entry(lang).or_insert(0);
+        let count = self.filecount.entry(lang).or_insert(0);
         *count += 1;
     }
 
-    fn crawl_dir(&mut self, dir: &Path) {
-        for entry in dir.read_dir().unwrap() {
-            let path = entry.unwrap().path();
-            if path.is_dir() {
-                self.crawl_dir(&path);
-            } else {
-                let lang;
-                if let Some(ext) = path.extension() {
-                    if let Some(ext) = ext.to_str() {
-                        lang = ext.to_string();
-                    } else {
-                        lang = "?".to_string();
-                    }
-                } else {
-                    lang = "?".to_string();
+    fn walk_dir(&mut self, dir: &Path) {
+        if let Ok(contents) = dir.read_dir() {
+            for entry in contents.map(|e| e.unwrap().path()) {
+                if entry.is_dir() {
+                    self.walk_dir(&entry);
                 }
-                self.add_file(lang.clone());
-                let reader = BufReader::new(File::open(path).unwrap());
+                else {
+                    let mut validutf8 = true;
+                    let mut lines = 0;
 
-                for line in reader.lines() {
-                    if let Ok(line) = line {
-                        if !line.is_empty() {
-                            self.add_line(lang.clone());
+                    if let Ok(f) = File::open(&entry) {
+                        for line in BufReader::new(f).lines() {
+                            if let Ok(_) = line {
+                                lines += 1;
+                            }
+                            else {
+                                validutf8 = false;
+                                break;
+                            }
                         }
+                    }
+                    else {
+                        eprintln!("{}: Access denied", entry.display());
+                        continue;
+                    }
+
+                    if validutf8 {
+                        let lang = match entry.extension() {
+                            Some(ext) => ext.to_os_string().into_string().unwrap(),
+                            None => String::from("?")
+                        };
+
+                        self.add_file(lang.clone());
+                        self.add_lines(lang, lines);
                     }
                 }
             }
         }
-    }
-}
-
-fn convert_extension(ext: &str) -> &'static str {
-    match ext {
-        "rs" => "Rust",
-        "py" => "Python",
-        "pyw" => "Python",
-        "js" => "JavaScript",
-        "html" => "HTML",
-        "css" => "CSS",
-        "sh" => "Shell",
-        "c" => "C",
-        "s" => "Assembly",
-        "asm" => "Assembly",
-        "cxx" => "C++",
-        "cpp" => "C++",
-        "h" => "C Header",
-        "hpp" => "C++ Header",
-        "java" => "Java",
-        "kt" => "Kotlin",
-        "go" => "Go",
-        "php" => "PHP",
-        "rb" => "Ruby",
-        "swift" => "Swift",
-        "vb" => "VisualBasic",
-        "vbs" => "VBScript",
-        "cs" => "C#",
-        "pl" => "Perl",
-        "lua" => "Lua",
-        "hs" => "Haskell",
-        "r" => "R",
-        "el" => "Emacs Lisp",
-        "clj" => "Clojure",
-        "cljc" => "Clojure",
-        "cljs" => "ClojureScript",
-        "ts" => "TypeScript",
-        "dart" => "Dart",
-        "scala" => "Scala",
-        "ml" => "OCaml",
-        "elm" => "Elm",
-        "erl" => "Erlang",
-        "m" => "Objective-C",
-        "mm" => "Objective-C++",
-        "tex" => "LaTeX",
-        "json" => "JSON",
-        "xml" => "XML",
-        "yml" => "YAML",
-        "toml" => "TOML",
-        "md" => "Markdown",
-        "txt" => "Txt",
-        "log" => "Log",
-        "jpg" => "JPEG",
-        "jpeg" => "JPEG",
-        "png" => "PNG",
-        "gif" => "GIF",
-        "svg" => "SVG",
-        "mp3" => "MP3",
-        "mp4" => "MP4",
-        "mkv" => "MKV",
-        "avi" => "AVI",
-        "mov" => "MOV",
-        "flv" => "FLV",
-        "wmv" => "WMV",
-        "zip" => "ZIP",
-        "zig" => "Zig",
-        "tar" => "TAR",
-        "gz" => "Gzip",
-        "xz" => "Xzip",
-        "7z" => "7-Zip",
-        "rar" => "RAR",
-        "pdf" => "PDF",
-        "doc" => "Word",
-        "docx" => "Word",
-        "xls" => "Excel",
-        "xlsx" => "Excel",
-        "ppt" => "PowerPoint",
-        "pptx" => "PowerPoint",
-        "exe" => "Executable",
-        "dll" => "DLL",
-        "so" => "Shared Object",
-        "o" => "Object",
-        "a" => "StaticLibrary",
-        "lib" => "StaticLibrary",
-        "class" => "JavaClass",
-        "jar" => "JavaArchive",
-        _ => "?"
+        else {
+            eprintln!("{}: Access denied", dir.display());
+        }
     }
 }
 
 fn main() {
     let argv: Vec<String> = env::args().collect();
-    let mut stats = Stats::new();
     let argc = argv.len();
+
     let path;
     if argc > 1 {
-        if argv[1] == "--raw" {
-            path = Path::new(".");
-        }
-        else {
-            path = Path::new(&argv[1]);
-        }
+        path = Path::new(&argv[1]);
         if !path.exists() {
-            eprintln!("Path does not exist: {}", &argv[1]);
+            eprintln!("{}: No such file or directory", &argv[1]);
             exit(1);
         }
         else if path.is_file() {
-            eprintln!("'{}' is a file, not a directory", &argv[1]);
+            eprintln!("{}: Item is a file, not a directory", &argv[1]);
             exit(1);
-        }
-        stats.crawl_dir(Path::new(path));
-    } else {
-        stats.crawl_dir(Path::new("."));
-    }
-
-    if argv.contains(&String::from("--raw")) {
-
-        let mut extensions = stats.line_count.keys().collect::<Vec<_>>();
-
-        extensions.sort();
-
-        println!("EXTENSION\tLINES\t        FILES");
-        for ext in extensions {
-            println!("{:<10}\t{:<8}\t{:<10}", ext, stats.line_count[ext], stats.file_count[ext]);
         }
     }
     else {
-        let extensions = stats.line_count.keys().collect::<Vec<_>>();
-        let mut unknown_lines = 0;
-        let mut unknown_files = 0;
-
-        println!("EXTENSION\tLINES\t        FILES");
-        println!("---------\t-----\t        -----");
-        for ext in extensions {
-            if convert_extension(ext) == "?" {
-                unknown_lines += stats.line_count[ext];
-                unknown_files += stats.file_count[ext];
-            }
-            else {
-                println!("{:<10}\t{:<8}\t{:<10}", convert_extension(ext), stats.line_count[ext], stats.file_count[ext]);
-            }
-        }
-        println!("{:<10}\t{:<8}\t{:<10}", "Other", unknown_lines, unknown_files);
+        path = Path::new(".");
     }
+
+    let mut stats = Stats::new();
+    stats.walk_dir(path);
+
+    let extensions = stats.linecount.keys().collect::<Vec<_>>();
+    let mut unknownfiles: usize = 0;
+    let mut unknownlines: usize = 0;
+
+    println!("EXTENSION\tLINES\t        FILES");
+    println!("---------\t-----\t        -----");
+
+    for ext in extensions {
+        let mapped = map_extension::map_extension(ext);
+
+        if mapped == "?" {
+            unknownlines += stats.linecount[ext];
+            unknownfiles += stats.filecount[ext];
+        }
+        else {
+            println!("{:<10}\t{:<8}\t{:<10}", mapped, stats.linecount[ext], stats.filecount[ext]);
+        }
+    }
+
+    println!("{:<10}\t{:<8}\t{:<10}", "Other", unknownlines, unknownfiles);
 }
